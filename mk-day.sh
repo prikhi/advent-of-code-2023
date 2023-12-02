03700616b9d3da2fb956802cc5b66a211824a1e5
Pavan Rikhi <pavan.rikhi@gmail.com>
2023-12-02 00:43:06 -0500

    Update Day Template File

--- mk-day.sh
+++ mk-day.sh
@@ -13,38 +13,41 @@ touch "inputs/day${DAY}.txt"
 
 cat >> "Day${DAY}.hs" <<EOF
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ViewPatterns #-}
+
 module Day${DAY} where
 
-import Control.Arrow    ((&&&))
+import Control.Arrow ((&&&))
 import Control.Monad
-import Data.Array       (Array)
+import Data.Array (Array)
 import Data.Bifunctor
 import Data.Char
 import Data.Either
 import Data.Foldable
-import Data.Function    (on)
+import Data.Function (on)
 import Data.Functor
-import Data.Map         (Map)
+import Data.Map (Map)
 import Data.Maybe
-import Data.MultiSet    (MultiSet)
-import Data.Set         (Set)
+import Data.MultiSet (MultiSet)
+import Data.Set (Set)
 import Text.ParserCombinators.ReadP
 
 import Harness
 import ParseHelper
 
-import qualified Data.Array as A
-import qualified Data.List as L
-import qualified Data.Map as M
-import qualified Data.MultiSet as MS
-import qualified Data.Set as S
+import Data.Array qualified as A
+import Data.List qualified as L
+import Data.Map qualified as M
+import Data.MultiSet qualified as MS
+import Data.Set qualified as S
 
 import Debug.Trace
 
