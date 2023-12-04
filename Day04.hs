b9c892f675beb533a596078161414d74e67d1092
Pavan Rikhi <pavan.rikhi@gmail.com>
2023-12-04 00:37:50 -0500

    Solve Day 4

---
+++ Day04.hs
@@ -0,0 +1,108 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Day04 where
+
+import Control.Arrow ((&&&))
+import Control.Monad
+import Control.Monad.ST
+import Data.Array (Array)
+import Data.Bifunctor
+import Data.Char
+import Data.Either
+import Data.Foldable
+import Data.Function (on)
+import Data.Functor
+import Data.Map (Map)
+import Data.Maybe
+import Data.MultiSet (MultiSet)
+import Data.Set (Set)
+import Text.ParserCombinators.ReadP
+
+import Harness
+import ParseHelper
+
+import Data.Array qualified as A
+import Data.List qualified as L
+import Data.Map qualified as M
+import Data.MultiSet qualified as MS
+import Data.Set qualified as S
+
+import Debug.Trace
+
+
+-- (parseInput lineParser) OR (parseInputRaw fullInputParser)
+main :: IO ()
+main = getInputAndSolve (parseInput parseCard) totalPointsWon countScratchCards
+
+
+-- SOLVE
+
+totalPointsWon :: [Card] -> Int
+totalPointsWon = sum . map calculatePoints
+
+
+countScratchCards :: [Card] -> Int
+countScratchCards = sum . A.elems . playCards
+
+
+-- HELPERS
+
+calculatePoints :: Card -> Int
+calculatePoints c =
+    let matches = c.winningNumbers `L.intersect` c.ourNumbers
+     in if null matches
+            then 0
+            else 2 ^ (length matches - 1)
+
+
+playCards :: [Card] -> A.Array Int Int
+playCards cards = runST $ do
+    arr <- A.newSTArray (1, maxIx) 1
+    foldM_ (playCard arr) () cards
+    A.freezeSTArray arr
+  where
+    maxIx :: Int
+    maxIx =
+        length cards
+    playCard :: A.STArray s Int Int -> () -> Card -> ST s ()
+    playCard arr _ card = do
+        let matches = card.winningNumbers `L.intersect` card.ourNumbers
+            nextIds =
+                [card.cardId + 1 .. card.cardId + length matches]
+        cardCount <- A.readSTArray arr card.cardId
+        forM_ nextIds $ \nextId -> when (nextId <= maxIx) $ do
+            nextCount <- A.readSTArray arr nextId
+            A.writeSTArray arr nextId (nextCount + cardCount)
+
+
+-- PARSE
+
+data Card = Card
+    { cardId :: Int
+    , winningNumbers :: [Int]
+    , ourNumbers :: [Int]
+    }
+    deriving (Show)
+
+
+parseCard :: ReadP Card
+parseCard = do
+    void $ string "Card" <* many1 parseSpace
+    cardId <- parseInt
+    void $ string ":" <* many1 parseSpace
+    winningNumbers <- sepBy1 parseInt (many1 parseSpace)
+    void $ many1 parseSpace *> string "|" <* many1 parseSpace
+    ourNumbers <- sepBy1 parseInt (many1 parseSpace)
+    return Card {..}
+  where
+    parseSpace :: ReadP Char
+    parseSpace = satisfy (== ' ')
